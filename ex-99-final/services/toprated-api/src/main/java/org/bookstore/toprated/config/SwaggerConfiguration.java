837dde4fe371314cea4604c4318602abcdaeb933
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-09-14 18:43:00 +0200

    Adding Open API Code Gen

--- ex-99-final/services/toprated-api/src/main/java/org/bookstore/toprated/config/SwaggerConfiguration.java
+++ ex-99-final/services/toprated-api/src/main/java/org/bookstore/toprated/config/SwaggerConfiguration.java
@@ -12,7 +12,9 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 
 @Configuration
 @EnableSwagger2
@@ -21,12 +23,14 @@ public class SwaggerConfiguration {
 
     @Bean
     public Docket apiDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("org.bookstore.toprated"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(getApiInfo());
+        docket.protocols(new HashSet<>(Arrays.asList("http", "https")));
+        return docket;
     }
 
     private ApiInfo getApiInfo() {
