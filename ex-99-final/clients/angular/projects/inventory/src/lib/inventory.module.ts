6c56d0ea919d2fed447c0b2c778f4cd4d43487d9
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-09-18 07:35:17 +0200

    Fixing main library pages

--- ex-99-final/clients/angular/projects/inventory/src/lib/inventory.module.ts
+++ ex-99-final/clients/angular/projects/inventory/src/lib/inventory.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { InventoryComponent } from './inventory.component';
 import { HomeComponent } from './home/home.component';
 import { BookComponent } from './book/book.component';
-import {InventoryRoutingModule} from './inventory-routing.module';
+import { InventoryRoutingModule } from './inventory-routing.module';
 
 @NgModule({
   imports: [
     InventoryRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    FontAwesomeModule
   ],
   declarations: [
     InventoryComponent, 
